d99db31731e7fe5d832d14056cb730c516dae355
Jean Paul Ordonez <jean.ordnnez@uao.edu.co>
2024-04-21 18:21:38 -0500

    Cors arreglado

---
+++ src/main/java/com/ecommerce/corsConfig/corsConfig.java
@@ -0,0 +1,17 @@
+package com.ecommerce.corsConfig;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class corsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*");
+    }
+}
